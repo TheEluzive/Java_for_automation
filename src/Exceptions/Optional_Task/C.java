5579e27ecf63d42c000a51fc9375a210beea9ef1
Evgeny Tkach <1eluzive1@gmail.com>
2019-07-01 13:50:05 +0400

    exception(optional)

---
+++ src/Exceptions/Optional_Task/C.java
@@ -0,0 +1,12 @@
+package Exceptions.Optional_Task;
+
+public class C extends B{
+    public C(String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
