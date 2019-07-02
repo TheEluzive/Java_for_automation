eda42b212772169b0d0ed2d77d85c9ac45a41c51
Evgeny Tkach <1eluzive1@gmail.com>
2019-07-02 16:52:13 +0400

    l9mbda(2 task)

--- src/Objects_Basic/Author.java
+++ src/Objects_Basic/Author.java
@@ -1,15 +1,50 @@
 package Objects_Basic;
 
-public class Author {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Author implements Comparator<Author> {
 
     protected String firstName;
     protected String lastName;
-    protected Author(String firstName, String lastName){
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+    public String getAuthor() {
+        return lastName + " " + firstName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(firstName, author.firstName) &&
+                Objects.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
     protected void print(){
     System.out.println("Author" + firstName + " " + lastName);
+    }
+
+    @Override
+    public int compare(Author o1, Author o2) {
+        if (o1.firstName.compareTo(o2.firstName) != 0 )
+        return o1.firstName.compareTo(o2.firstName);
+        else return o1.lastName.compareTo(o2.lastName);
     }
 }
--- src/Objects_Basic/Book.java
+++ src/Objects_Basic/Book.java
@@ -1,12 +1,47 @@
 package Objects_Basic;
 
-public class Book {
+import java.util.Comparator;
+
+public class Book   {
     private String title;
     private Author author;
     private double price;
-    protected Book(String title, Author author, double price){
+    public Book(String title, Author author, double price){
         this.title = title;
         this.author = author;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author.toString() +
+                ", price=" + price +
+                '}';
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 
@@ -14,5 +49,12 @@ public class Book {
         System.out.println("Title: " + title);
         author.print();
         System.out.println("Price: " + price + " $");
+    }
+
+
+
+
+    public double compare(Book o1, Book o2) {
+        return this.getPrice() - o2.getPrice();
     }
 }
